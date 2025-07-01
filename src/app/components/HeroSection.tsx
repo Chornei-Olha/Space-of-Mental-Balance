0c5f890cf8361a2c756742e1f40540c82c4f83d4
workProject085 <workProject085@gmail.com>
2025-07-01 10:30:03 +0300

    commit

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -58,7 +58,7 @@ export default function HeroSection() {
         muted={isMuted}
         className="absolute top-0 left-0 w-full h-full object-cover z-0 scale-[1.41]"
       >
-        <source src="/assets/video.mp4" type="video/mp4" />
+        <source src="/assets/video-cut.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
